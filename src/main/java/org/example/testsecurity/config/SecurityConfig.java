1dd34fa1d14154884555c1edd272c37de5194c0c
shincheolhui <shinchulhee0614@gmail.com>
2024-10-25 00:20:59 +0900

    커스텀 로그인 페이지 csrf disable

--- src/main/java/org/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/testsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,14 @@ public class SecurityConfig {
                 .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated()
         );
+
+        http.formLogin((form) -> form
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .permitAll()
+        );
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
